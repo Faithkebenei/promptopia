224e4cdb0361127acce8bde027381743ca4ce748
Faithkebenei <faithkebs16@gmail.com>
2023-10-23 14:12:51 +0300

    Submitting prompt ui

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+'use client';
+
+import {useState, useEffect} from 'react'
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div></div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+    }
+  }, [])
+   
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+        type='text'
+        placeholder='Search for a tag or username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList 
+      data={[]}
+      handleTagClick={() => {}}/>
+    </section>
   )
 }
 
